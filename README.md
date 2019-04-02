fb59254cf259c90cf0c04d7c31475d56b75904d5
Michiel Magendans <michiel_m@live.nl>
2019-04-02 13:32:25 +0200

    Implemented queue and dynamic database loading

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# node-red-contrib-queued-sqlite
-An SQLite node that queues queries before executing them.
+node-red-node-sqlite
+====================
+
+A Node-Red node to read and write a local sqlite database.
+
+Based on the basic node-red-sqlite, but this allows queueing of messages and dynamic databases.
+
+Install
+-------
+
+Run the following command in your Node-RED user directory - typically `~/.node-red`
+
+    npm i --unsafe-perm node-red-contrib-queued-sqlite
+
+
+Usage
+-----
+
+Allows basic access to a Sqlite database.
+
+This node uses the <b>db.all</b> operation against the configured database.
+This does allow INSERTS, UPDATES and DELETES.
+
+By it's very nature it is SQL injection... so *be careful* out there...
+
+`msg.topic` must hold the <i>query</i> for the database, and the result is returned in `msg.payload`.
+
+All queries are automatically queued and dequeued per database.
+
+Typically the returned payload will be an array of the result rows, (or an error).
+
+You can load sqlite extensions by inputting a <code>msg.extension</code> property containing the full path and filename.
+
+You can choose a database in the configuration or provide it in `msg.database`.
+
+The reconnect timeout in milliseconds can be changed by adding a line to **settings.js**
+
+    sqliteReconnectTime: 20000,
---
+++ sqlite.js
@@ -0,0 +1,307 @@
+module.exports = function(RED) {
+    "use strict";
+
+
+    var Database = function(name, config, node)
+    {
+        var reconnect = RED.settings.sqliteReconnectTime || 20000;
+        var sqlite3 = require('sqlite3');
+
+        this.dbname = name || config.db;
+        this.mod = config.mode;
+
+        if (config.mode === "RWC") { this.mode = sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE; }
+        if (config.mode === "RW") { this.mode = sqlite3.OPEN_READWRITE; }
+        if (config.mode === "RO") { this.mode = sqlite3.OPEN_READONLY; }
+
+        var self = this;
+
+        this.doConnect = function(callback) {
+            self.db = self.db || new sqlite3.Database(self.dbname,self.mode);
+            self.db.on('open', function() {
+                if (node.tick) { clearTimeout(node.tick); }
+                node.log("opened "+self.dbname+" ok");
+                callback(null);
+            });
+            self.db.on('error', function(err) {
+                node.error("failed to open "+self.dbname, err);
+                node.tick = setTimeout(function() { self.doConnect(); }, reconnect);
+                callback(err);
+            });
+        }
+
+        this.queue = [];
+        this.busy = false;
+
+        this.doQuery = function(query, params, callback, batch = false)
+        {
+            this.queue.push({query: query, params: params, callback: callback, batch: batch});
+            this.processQueue();
+        };
+
+        this.process = function(q) {
+            if (q === undefined)
+            {
+                this.busy = false;
+                return;
+            }
+            var self = this;
+            if (q.batch)
+            {
+                self.db.exec(q.query, function(err) {
+                    if (err) { 
+                        q.callback(err);
+                    }
+                    else {
+                        q.callback(null);
+                    }
+                    q = self.queue.shift();
+                    self.process(q);
+                })
+            }
+            else
+            {
+                node.log("Doing query " + q.query);
+                self.db.all(q.query, q.params, function(err, row) {
+                    if (err) { 
+                        q.callback(err)
+                    }
+                    else {
+                        q.callback(null, row)
+                    }
+                    q = self.queue.shift();
+                    self.process(q);
+                });
+            }
+
+
+        };
+
+        this.processQueue = function() {
+            if (this.busy) {
+                return;
+            }
+            this.busy = true;
+
+            if (this.queue.length === 0)
+            {
+                this.busy = false;
+                return;
+            }
+            var q = this.queue.shift();
+            this.process(q);
+        };
+
+        this.loadExtension = function(extension, callback) {
+            self.db.loadExtension(extension, callback);
+        };
+    }
+
+    function SqliteNodeDB(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.databases = {};
+
+        node.on('close', function (done) {
+            if (node.tick) { clearTimeout(node.tick); }
+            for(let key in node.databases)
+            {
+                if (node.databases[key].db)
+                    node.databases[key].db.close(); 
+            }
+            done();
+        });
+
+        node.get = function(name, callback = function() {})
+        {
+            if (this.databases[name] === undefined || this.databases[name] === null)
+            {
+                this.databases[name] = new Database(name, config, node);
+                var self = this;
+                this.databases[name].doConnect(function(err) {
+                    if (err) {
+                        callback(err, null)
+                        delete self.databases[name];
+                    } else {
+                        callback(null, self.databases[name])
+                    }
+                });
+            }
+            else
+            {
+                callback(null, this.databases[name]);
+            }
+        }
+    }
+
+    RED.nodes.registerType("sqliteQueuedDb",SqliteNodeDB);
+
+
+    function SqliteNodeIn(config) {
+        RED.nodes.createNode(this,config);
+        this.dbName = config.dbName||"msg.database";
+        this.mydb = config.mydb;
+        this.db = config.db;
+        this.sqlquery = config.sqlquery||"msg.topic";
+        this.sql = config.sql;
+        this.mydbConfig = RED.nodes.getNode(this.mydb);
+        var node = this;
+        node.status({});
+
+        this.database = null;
+
+
+        if (node.mydbConfig) {
+            node.status({fill:"green",shape:"dot",text:this.mydbConfig.mode});
+           
+            if (node.dbName !== "msg.database") {
+                node.mydbConfig.get(node.db, function(err, db) {
+                    node.log("opened database in advance");
+                    node.database = db;
+                });
+            }
+            var bind = [];
+
+
+            var processQuery = function(msg, db) {
+                if (db === undefined) 
+                {
+                    node.log("db is undefined")
+                }
+                if (node.sqlquery == "msg.topic"){
+                    if (typeof msg.topic === 'string') {
+                        if (msg.topic.length > 0) {
+                            bind = Array.isArray(msg.payload) ? msg.payload : [];
+
+                            db.doQuery(msg.topic, bind, function(err, row) {
+                                if (err) { node.error(err,msg); }
+                                else {
+                                    msg.payload = row;
+                                    node.send(msg);
+                                }
+                            });
+                        }
+                    }
+                    else {
+                        node.error("msg.topic : the query is not defined as a string",msg);
+                        node.status({fill:"red",shape:"dot",text:"msg.topic error"});
+                    }
+                }
+                if (node.sqlquery == "batch") {
+                    if (typeof msg.topic === 'string') {
+                        if (msg.topic.length > 0) {
+                            db.doQuery(msg.topic, null, function(err) {
+                                if (err) { node.error(err,msg);}
+                                else {
+                                    msg.payload = [];
+                                    node.send(msg);
+                                }
+                            }, true);
+                        }
+                    }
+                    else {
+                        node.error("msg.topic : the query is not defined as string", msg);
+                        node.status({fill:"red", shape:"dot",text:"msg.topic error"});
+                    }
+                }
+                if (node.sqlquery == "fixed"){
+                    if (typeof node.sql === 'string') {
+                        if (node.sql.length > 0) {
+                            db.doQuery(node.sql, bind, function(err, row) {
+                                if (err) { node.error(err,msg); }
+                                else {
+                                    msg.payload = row;
+                                    node.send(msg);
+                                }
+                            });
+                        }
+                    }
+                    else{
+                        if (node.sql === null || node.sql == "") {
+                            node.error("SQL statement config not set up",msg);
+                            node.status({fill:"red",shape:"dot",text:"SQL config not set up"});
+                        }
+                    }
+                }
+                if (node.sqlquery == "prepared"){
+                    if (typeof node.sql === 'string' && typeof msg.params !== "undefined" && typeof msg.params === "object") {
+                        if (node.sql.length > 0) {
+                            db.doQuery(node.sql, msg.params, function(err, row) {
+                                if (err) { node.error(err,msg); }
+                                else {
+                                    msg.payload = row;
+                                    node.send(msg);
+                                }
+                            });
+                        }
+                    }
+                    else {
+                        if (node.sql === null || node.sql == "") {
+                            node.error("Prepared statement config not set up",msg);
+                            node.status({fill:"red",shape:"dot",text:"Prepared statement not set up"});
+                        }
+                        if (typeof msg.params == "undefined") {
+                            node.error("msg.params not passed");
+                            node.status({fill:"red",shape:"dot",text:"msg.params not defined"});
+                        }
+                        else if (typeof msg.params != "object") {
+                            node.error("msg.params not an object");
+                            node.status({fill:"red",shape:"dot",text:"msg.params not an object"});
+                        }
+                    }
+                }
+            };
+            var doQuery = function(msg) {
+                if (node.dbName === "msg.database") 
+                {
+                    if (typeof msg.database === 'string')
+                    {
+                        var db = node.mydbConfig.get(msg.database, function(err, db) {
+                            if (!err) {
+                                if (msg.hasOwnProperty("extension")) {
+                                    db.loadExtension(msg.extension, function(err) {
+                                        if (err) { node.error(err,msg); }
+                                        else { processQuery(msg, db); }
+                                    });
+                                }
+                                else { processQuery(msg, db); }
+                            }
+                        });
+                    } 
+                    else 
+                    {
+                        node.error("msg.database : the database name is not defined as string", msg);
+                        node.status({fill:"red", shape:"dot",text:"msg.database error"});
+                    }
+                }
+                else
+                {
+                    if (node.database !== null)
+                    {
+                        if (msg.hasOwnProperty("extension")) {
+                            node.database.loadExtension(msg.extension, function(err) {
+                                if (err) { node.error(err,msg); }
+                                else { processQuery(msg, node.database); }
+                            });
+                        }
+                        else { processQuery(msg, node.database); }
+                    }
+                    else
+                    {
+                        node.error("Sqlite database not configured correctly");
+                    }
+                }
+
+                
+            }
+
+            node.on("input", function(msg) {
+                doQuery(msg);
+            });
+        }
+        else {
+            node.error("Sqlite database not configured");
+        }
+    }
+    RED.nodes.registerType("queuedsqlite",SqliteNodeIn);
+}
